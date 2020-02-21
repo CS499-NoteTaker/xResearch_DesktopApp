ca1169c1b474338c24c5f0db30a8c74ab2ae6d58
Joshua Moran <43650831+JoshuaMoran@users.noreply.github.com>
2020-02-21 01:23:38 -0800

    Added Electron Files

---
+++ main.js
@@ -0,0 +1,20 @@
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+
+const {app, BrowserWindow} = electron;
+
+let mainWindow;
+
+// Listen for app to be ready
+app.on('ready', function (){
+    // Create new window 
+    mainWindow = new BrowserWindow({});
+
+    // Load html into window
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file',
+        slashes: true
+    }));
+});
