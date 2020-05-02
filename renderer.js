d82e79d0ea3202b0ea268cadad807836c363cbba
Joshua <moranja@plu.edu>
2020-05-01 22:49:40 -0700

    Removing method that converts string to html, was not nexessary

--- renderer.js
+++ renderer.js
@@ -94,38 +94,6 @@ var LoadResearchedCell = function(e) {
     }
 }
 
-var support = (function() {
-    if (!window.DOMParser) return false;
-    var parser = new DOMParser();
-    try {
-        parser.parseFromString('x', 'text/html');
-    } catch (err) {
-        return false;
-    }
-    return true;
-})();
-
-/**
- * Convert a template string into HTML DOM nodes
- * @param  {String} str The template string
- * @return {Node}       The template HTML
- */
-var stringToHTML = function(str) {
-
-    // If DOMParser is supported, use it
-    if (support) {
-        var parser = new DOMParser();
-        var doc = parser.parseFromString(str, 'text/html');
-        return doc.body;
-    }
-
-    // Otherwise, fallback to old-school method
-    var dom = document.createElement('div');
-    dom.innerHTML = str;
-    return dom;
-
-};
-
 
 
 //summarize the selected text by the user
